b1cbad27ebc23e09a388dd1516911835e9387ac8
Arbon Kastrati <arbon_kastrati@outlook.com>
2026-07-27 14:16:22 +0200

    create proxy server, so the request to the api.github.com are set as comming from this very domain, include the access token into the request

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: {
+            proxy: {
+                '/api/github': {
+                    target: 'https://api.github.com',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api\/github/, ''),
+                    headers: {
+                        Authentication: `Bearer ${env.VITE_GITHUB_API_TOKEN}`,
+                        Accept: 'application/vnd.github+json',
+                    },
+                },
+            },
+        },
+    };
 });
