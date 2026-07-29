f27fc3d6e96c9800b973739cd0be2615150d55d2
Arbon Kastrati <arbon_kastrati@outlook.com>
2026-07-29 14:20:56 +0200

    update type User, and add SuggestedUser and SuggestedUserProps types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,23 @@
+import type { Dispatch, SetStateAction } from 'react';
+
 export type User = {
     name: string;
+    login: string;
     avatar_url: string;
     html_url: string;
     bio: string;
+};
+
+export type SuggestedUser = {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+};
+
+export type SuggestedUserProps = {
+    suggestions: SuggestedUser[];
+    setDismissSuggestions: Dispatch<SetStateAction<boolean>>;
+    selectSuggestion: (user: string) => void;
 };
 
 export type RecentSearchesProps = {
